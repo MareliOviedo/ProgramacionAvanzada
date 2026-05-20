f6c27671e1f21f93e8dc3b9d038abc76ba95d5de
MareliOviedo <a2243330369@alumnos.uat.edu.mx>
2026-05-20 17:19:02 -0600

    Add files via upload

---
+++ JavaOracle/src/ejercicios/Text01.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class Text01 {
+    public static void main(String[] args) {
+        System.out.println("  /\\           /\\  ");System.out.println(" /  \\_________/  \\ ");
+    System.out.println("/                 \\");
+            System.out.println("(  /\\         /\\  )");
+    System.out.println("====     V     ====");
+            System.out.println("======(__|__)======");
+    System.out.println(" (               ) ");
+
+            System.out.println("  (___________)  ");
+    }
+}
---
+++ JavaOracle/src/ejercicios/Text03.java
@@ -0,0 +1,21 @@
+package ejercicios;
+
+public class Text03 {
+    public static void drawLegs() {
+        System.out.println("     ||      ||     ");
+        System.out.println("     ||      ||     ");
+        System.out.println("    (||)    (||)    ");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("  /\\           /\\  ");
+        System.out.println(" /  \\_________/  \\ ");
+        System.out.println("/                 \\");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println(" (               ) ");
+        System.out.println("  (___________)  ");
+        drawLegs();
+    }
+}
